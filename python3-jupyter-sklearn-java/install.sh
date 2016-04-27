94cd6832a56fce7bc6969f931ee62b9f10f32f4c
Fabian Linzberger <fabian@sellpy.se>
2016-04-27 19:25:01 +0200

    newer java in ubuntu xenial

--- python3-jupyter-sklearn-java/install.sh
+++ python3-jupyter-sklearn-java/install.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y --no-install-recommends openjdk-7-jre-headless
+apt-get install -y --no-install-recommends openjdk-8-jre-headless
 apt-get clean
