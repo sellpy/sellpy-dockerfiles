81c776d23a441f5bb5acb6b5726f21a387c14518
Andreas Drangel <andreas.drangel@sellpy.se>
2017-09-25 16:51:37 +0200

    intermec drivers

--- cups/install.sh
+++ cups/install.sh
@@ -4,5 +4,14 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y cups
+apt-get install -y \
+  cups \
+  automake \
+  autoconf \
+  gcc \
+  ghostscript \
+  poppler-utils \
+  netpbm \
+  build-essential \
+  sudo
 apt-get clean
