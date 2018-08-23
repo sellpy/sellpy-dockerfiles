b573e7eb300b6fd93a793852f5928755402e120a
Johan Arnör <johan.arnor@gmail.com>
2018-08-23 10:44:35 +0200

    Attempt to lock cups version

--- cups/install.sh
+++ cups/install.sh
@@ -4,8 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade -y
+# locked cups version since 2.2.7-1ubuntu2.1 for some reason didn't delete the data job files in /spool
 apt-get install -y \
-  cups \
+  cups=2.1.3-4ubuntu0.4 \
   automake \
   autoconf \
   gcc \
