aa4cd03db7c8ff208b30548e0bd09b0702287228
Johan Arnör <johan.arnor@gmail.com>
2018-08-23 11:20:16 +0200

    Revert "Attempt to lock cups version"
    This reverts commit b573e7eb300b6fd93a793852f5928755402e120a.

--- cups/install.sh
+++ cups/install.sh
@@ -4,9 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade -y
-# locked cups version since 2.2.7-1ubuntu2.1 for some reason didn't delete the data job files in /spool
 apt-get install -y \
-  cups=2.1.3-4ubuntu0.4 \
+  cups \
   automake \
   autoconf \
   gcc \
